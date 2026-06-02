6a318cb483e4abf05b99eb052b170ddb31400cc1
parashars16 <shreyaparashar06@gmail.com>
2026-06-02 16:58:53 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,26 @@
+# variable 
+# string
+first_name = "Shreya"
+print(first_name)
+print(f"Hello {first_name}")
+
+#integers 
+age= 25 
+quantity= 3.5
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+
+#float
+price = 10.99
+gpa = 3.2
+
+print(f"The price is ${price}")
+print(f"Your gpa is :{gpa}")
+
+#boolean
+Is_student = True
+if Is_student :
+  print("You are a student")
+else:
+  print("You are not a student")
