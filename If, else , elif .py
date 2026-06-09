c45597be8a861e208419f7cbc0a6f756e4955565
parashars16 <shreyaparashar06@gmail.com>
2026-06-09 19:00:27 +0530

    Add files via upload

---
+++ If, else , elif .py
@@ -0,0 +1,18 @@
+age = int(input("Enter your age ="))
+
+if age>= 100 :
+    print("You are too old!")
+elif age>=18:
+    print("Your are now signed up!")
+else:
+    print("You must be 18+")
+
+    #example 2 
+    response = input("Would you like some food?(Y/N):")
+
+    if response =="Y":
+    print("Have some food!")
+else:
+print("No food for you!")
+
+
